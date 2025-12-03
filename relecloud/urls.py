185e8829588a990243a6a592d2efdbd4ca9c749c
Javier de Lorenzo <9306503@alumnos.ufv.es>
2025-12-04 00:12:00 +0100

    AB#93 Probar los flujos de login/logout y arreglar errores en los mismos

--- relecloud/urls.py
+++ relecloud/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('info_request', views.InfoRequestCreate.as_view(), name='info_request'),
     path('registro/', views.RegistroUsuarioCreate.as_view(), name='registro'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
