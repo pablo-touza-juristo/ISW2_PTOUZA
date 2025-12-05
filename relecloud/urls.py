d2742877eb0e8af4161195bd94f1470e083c0461
ivanfrancisco-lopez-garcia <ivanfranciscolopezgarcia@gmail.com>
2025-12-05 11:41:59 +0100

    AB#7 Configurar manejo de archivos media en Django

--- relecloud/urls.py
+++ relecloud/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('registro/', views.RegistroUsuarioCreate.as_view(), name='registro'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', include('relecloud.urls')),
+    path('admin/', admin.site.urls),
 ]
